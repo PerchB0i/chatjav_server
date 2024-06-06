8dd2352c3dcd995855e4fbe44780a5fb70fd5f78
KaminaPL <malytankus@gmail.com>
2024-06-06 15:20:43 +0200

    init vol2

--- src/main/java/org/example/Message.java
+++ src/main/java/org/example/Message.java
@@ -3,4 +3,11 @@ package org.example;
 public class Message {
     public MessageType type;
     public String content;
+
+    public Message() {}
+
+    public Message(MessageType type, String content) {
+        this.type = type;
+        this.content = content;
+    }
 }
